72e4be8573455a9472fe9ffc7afa52e4c3048737
shjang7 <lucy.sh.jang@gmail.com>
2020-01-22 15:31:22 +0900

    Use react for view, add app component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting!
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
