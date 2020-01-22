83b66a1b8d99f73415ea565b0f8488059784f062
shjang7 <suhy.jang.shj@gmail.com>
2020-01-22 15:08:55 +0900

    Move link controller in api directory

--- app/controllers/links_controller.rb
+++
@@ -1,4 +0,0 @@
-class LinksController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'links/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :links, only: [:index]
+    end
+  end
 end
