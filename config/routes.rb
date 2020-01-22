ae0908beb682a2dec187d2d1163abae0e09ffe7e
shjang7 <suhy.jang.shj@gmail.com>
2020-01-22 15:31:22 +0900

    Use react for view, add app component

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/index'
   namespace :api do
     namespace :v1 do
       resources :links, only: [:index]
     end
   end
+
+  root 'static#index'
 end
