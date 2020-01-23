0c53d5728af4f5f1f5f09ef5b194440414b0a264
shjang7 <lucy.sh.jang@gmail.com>
2020-01-23 15:29:37 +0900

    Add the sessions controller, End session back-end side

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   get 'static/index'
+  
   namespace :api do
     namespace :v1 do
       resources :links, only: [:index]
       resources :users, only: %i[create show index]
+
+      post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+      post '/logged_in', to: 'sessions#is_logged_in?'
     end
   end
 
