80398a148e4a4c014585e8a35f05ed4693550557
shjang7 <suhy.jang.shj@gmail.com>
2020-01-23 14:15:35 +0900

    Add user model, controller and bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :links, only: [:index]
+      resources :users, only: %i[create show index]
     end
   end
 
