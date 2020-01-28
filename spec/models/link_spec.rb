2d65b723b91fad7dacf573a89eac7b1bf3eed8c2
shjang7 <suhy.jang.shj@gmail.com>
2020-01-28 15:18:29 +0900

    Add model create, delete rspec test

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,59 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  context 'with valid attributes' do
-    # let(:link)
-    # before do
-    #   @user = create(:user)
-    #   login!
-    # end
-    #
-    # it 'has correct informations' do
-    #   link = Link.new(
-    #     title: 'sample title',
-    #     url: 'www.example.com',
-    #   )
-    #   expect(link).to be_valid
-    # end
-    #
-    # it 'has a right factory' do
-    #   expect(build(:link)).to be_valid
-    # end
+  context 'with no user' do
+    it 'fail at create' do
+      link = Link.new( attributes_for(:link) )
+      expect(link).to be_invalid
+    end
   end
 
-  # context 'with invalid attributes for blank' do
-  #   it 'has blank username' do
-  #     user = build(:user, username: '')
-  #     expect(user).to_not be_valid
-  #   end
-  #
-  #   it 'has blank email' do
-  #     user = build(:user, email: '')
-  #     expect(user).to_not be_valid
-  #   end
-  #
-  #   it 'has blank password' do
-  #     user = build(:user, password: '')
-  #     expect(user).to_not be_valid
-  #   end
-  # end
-  #
-  # context 'with invalid attributes for wrong info' do
-  #   it 'is invalid with duplicate username' do
-  #     mos = create(:user)
-  #     jen = build(:user, email: mos.username)
-  #     expect(jen).to_not be_valid
-  #   end
-  #
-  #   it 'is invalid with duplicate email' do
-  #     mos = create(:user)
-  #     jen = build(:user, email: mos.email)
-  #     expect(jen).to_not be_valid
-  #   end
-  #
-  #   it 'is invalid with short username under 4 characters' do
-  #     user = build(:user, username: '123')
-  #     expect(user).to_not be_valid
-  #   end
-  # end
+  context 'with user' do
+    let(:user) { create(:user) }
+
+    it 'is valid, create, and destroy' do
+      link = user.links.build( attributes_for(:link) )
+      expect(link).to be_valid
+      link.save!
+      expect do
+        delete_link = link.destroy
+      end.to change(Link, :count).by(-1)
+    end
+
+    it 'is invalid with blank title' do
+      link = user.links.build( attributes_for(:link, title: '') )
+      expect(link).to be_invalid
+    end
+
+    it 'is invalid with blank url' do
+      link = user.links.build( attributes_for(:link, url: '') )
+      expect(link).to be_invalid
+    end
+  end
 end
