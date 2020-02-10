406877fa4db0e94db5986fa941b3942ba06787da
shjang7 <lucy.sh.jang@gmail.com>
2020-02-11 01:56:31 +0900

    Add not found page, bg color

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import { Redirect } from 'react-router-dom';
+import PropTypes from "prop-types";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from '../configure_store';
@@ -7,6 +8,7 @@ import LinksList from '../container/links/links_list';
 import ShowLink from '../container/links/show_link';
 import Signup from '../container/registrations/signup';
 import Login from '../container/sessions/login';
+import NotFound from './not_found';
 
 const store = configureStore();
 
@@ -19,6 +21,8 @@ const App = (props) => {
           <Route exact path="/links/:id" render={ props => <ShowLink {...props} /> } />
           <Route exact path="/login" render={ props => <Login {...props} /> } />
           <Route exact path="/signup" render={ props => <Signup {...props} /> } />
+          <Route exact path="/not_found" render={ props => <NotFound {...props} /> } />
+          <Redirect to='/not_found' />
         </Switch>
       </BrowserRouter>
     </Provider>
