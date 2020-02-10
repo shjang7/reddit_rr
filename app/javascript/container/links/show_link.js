6e7eae53e72b1a83b681205b687f6aa0cf2e441a
shjang7 <lucy.sh.jang@gmail.com>
2020-02-10 16:24:48 +0900

    Move presentational component for a link

--- app/javascript/container/links/show_link.js
+++ app/javascript/container/links/show_link.js
@@ -6,24 +6,13 @@ import Navbar from '../navbar';
 import NewLinks from './new_links';
 import Comments from '../comments/comments';
 import { destroyLink, readLink, upvoteLink, downvoteLink } from '../../actions';
-import { timeSince } from '../../common/functions';
+import ALink from '../../components/links/a_link';
 
 const ShowLink = (props) => {
   const { history, location, match: { params: { id: linkId } }, errors, links, destroyLink, readLink, upvoteLink, downvoteLink } = props;
-  const [link, setLink] = useState(null);
-  const [upvote, setUpvote] = useState(null);
-  const [downvote, setDownvote] = useState(null);
-  const [voteweight, setVoteWeight] = useState(null);
   useEffect(() => {
     readLink(linkId);
   }, []);
-
-  useEffect(() => {
-    setLink(links.link);
-    setUpvote(links.link.votes.up);
-    setDownvote(links.link.votes.down);
-    setVoteWeight(links.link.votes.weight);
-  }, [links.link]);
 
   const redirect = () => {
     history.push('/');
@@ -42,27 +31,13 @@ const ShowLink = (props) => {
     downvoteLink(linkId);
   }
 
-  const renderLink = link ? (
-      <div>
-        { link.link.title }
-        { timeSince(link.link.created_at) }
-        { link.url }
-        <button type='button' onClick={ () => handleDelete() }>delete</button>
-      </div>
-    )
-    : null;
-
   return (
     <React.Fragment>
       <Navbar history={ history }/>
       <Link to='/'>main</Link>
       <div>{ errors }</div>
       <h1>Show Link</h1>
-      <div>{ renderLink }</div>
-      <div>{ links.link.author }</div>
-      <button type='button' onClick={ () => handleUpvote() }>upvote</button>
-      <button type='button' onClick={ () => handleDownvote() }>downvote</button>
-      <div>{ upvote } { downvote } { voteweight }</div>
+      <ALink data={links.link} handleUpvote={handleUpvote} handleDownvote={handleDownvote} handleDelete={handleDelete} />
       <Comments linkId={ linkId } />
     </React.Fragment>
   );
