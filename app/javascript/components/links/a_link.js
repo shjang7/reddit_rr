aabae146b829cec21010fe7b50a918d5c7017973
shjang7 <suhy.jang.shj@gmail.com>
2020-02-11 20:11:11 +0900

    Add react icons

--- app/javascript/components/links/a_link.js
+++ app/javascript/components/links/a_link.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { timeSince } from '../../common/functions';
+import { FaTwitterSquare } from "react-icons/lib/fa";
 
 const ALink = ({ data, handleUpvote, handleDownvote, handleDelete, titleTo }) => {
   const { author, created_at, id, title, updated_at, url, user_id, votes} = data;
@@ -35,8 +36,11 @@ const ALink = ({ data, handleUpvote, handleDownvote, handleDelete, titleTo }) =>
       <div>
         { id }
         <span>id</span>
-        <button type='button' onClick={ () => handleUpvoteLocal() }>upvote</button>
-        { votes.up }
+        <a type='button' className="btn btn-default btn-sm" rel="nofollow" onClick={ () => handleUpvoteLocal() }>
+          <FaTwitterSquare />
+          upvote
+          { votes.up }
+        </a>
         <button type='button' onClick={ () => handleDownvoteLocal() }>downvote</button>
         { votes.down }
         <span>total</span>
