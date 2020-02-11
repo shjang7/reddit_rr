bed9bd42ad52e019d9e3c376ab398381954a2bed
shjang7 <suhy.jang.shj@gmail.com>
2020-02-11 20:41:21 +0900

    Update link buttons

--- app/javascript/components/links/a_link.js
+++ app/javascript/components/links/a_link.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { timeSince } from '../../common/functions';
-import { FaTwitterSquare } from "react-icons/lib/fa";
+import { FaChevronUp, FaChevronDown } from 'react-icons/lib/fa';
 
 const ALink = ({ data, handleUpvote, handleDownvote, handleDelete, titleTo }) => {
   const { author, created_at, id, title, updated_at, url, user_id, votes} = data;
@@ -29,22 +29,30 @@ const ALink = ({ data, handleUpvote, handleDownvote, handleDelete, titleTo }) =>
     <div>
       <div>
         { renderTitle }
-        { timeSince(created_at) } { url }
-        <button type='button' onClick={ () => handleDeleteLocal() }>delete</button>
+        { timeSince(created_at) }
+        <button type='button' onClick={ () => handleDeleteLocal() }>
+          delete
+        </button>
       </div>
       <div>{ author }</div>
-      <div>
-        { id }
-        <span>id</span>
-        <a type='button' className="btn btn-default btn-sm" rel="nofollow" onClick={ () => handleUpvoteLocal() }>
-          <FaTwitterSquare />
-          upvote
+      <div className='buttons'>
+        <a type='button' className="btn btn-base btn-sm" rel="nofollow" href={ url }>
+          <span>Visit Link</span>
+        </a>
+        <a type='button' className="btn btn-base btn-sm vote" rel="nofollow" onClick={ () => handleUpvoteLocal() }>
+          <FaChevronUp />
+          <span>upvote</span>
           { votes.up }
         </a>
-        <button type='button' onClick={ () => handleDownvoteLocal() }>downvote</button>
-        { votes.down }
-        <span>total</span>
-        { votes.weight }
+        <a type='button' className="btn btn-base btn-sm vote" rel="nofollow" onClick={ () => handleDownvoteLocal() }>
+          <FaChevronDown />
+          <span>downvote</span>
+          { votes.down }
+        </a>
+        <span className="btn-sm vote">
+          <span>total</span>
+          { votes.weight }
+        </span>
       </div>
     </div>
   )
