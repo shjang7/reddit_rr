8cd3a5c6e44eeae931fb74f8dfa7f5b9b84928e6
shjang7 <suhy.jang.shj@gmail.com>
2020-02-11 22:00:29 +0900

    Add author name to send comment from backend

--- app/javascript/components/comments/comment.js
+++ app/javascript/components/comments/comment.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { timeSince } from '../../common/functions';
 
 const Comment = ({ data, handleDelete }) => {
-  const { body, created_at, user_id } = data;
+  const { id, body, created_at, user_id, author } = data;
 
   return (
     <>
-      { data.body }
-      | { timeSince(data.created_at) }
-      | user id: { data.user_id }
-      <button type='button' className="btn btn-base btn-sm" onClick={ () => handleDelete(data.id) }>
+      { body }
+      | { timeSince(created_at) }
+      | by { author }
+      <button type='button' className="btn btn-base btn-sm" onClick={ () => handleDelete(id) }>
         delete
       </button>
     </>
