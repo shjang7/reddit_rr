72b374a57a29cc74d6adbe698484a0906175d718
shjang7 <suhy.jang.shj@gmail.com>
2020-02-14 06:14:05 +0900

    Add navbar style

--- app/javascript/container/navbar.js
+++ app/javascript/container/navbar.js
@@ -19,30 +19,34 @@ const Navbar = ({ session, destroySession, readSession }) => {
   }
 
   const userLinks = () => (
-    <div>
-      { uname ? 'login with: ' + uname.username : null }
-      <Link to='/links/new'>Write Link</Link>
-      <a type='button' className="btn btn-base btn-sm" onClick={ () => handleLogout() }>
-        Log Out
-      </a>
-      <hr />
-    </div>
+    <>
+      { uname ? <li className="nav-item active"><span>login with: {uname.username} </span></li> : null }
+      <li className="nav-item"><Link to='/links/new'>Write Link</Link></li>
+      <li className="nav-item" onClick={ () => handleLogout() }>
+        <span>Log Out</span>
+      </li>
+    </>
   );
 
   const guestLinks = () => (
-    <div>
-      <Link to='/login'>Log In</Link>
-      <br />
-      <Link to='/signup'>Sign Up</Link>
-      <br />
-      <hr />
-    </div>
+    <>
+      <li className="nav-item active"><Link to='/login'>Log In</Link></li>
+      <li className="nav-item"><Link to='/signup'>Sign Up</Link></li>
+    </>
   );
 
   return (
-    <>
-      { uname.username ? userLinks() : guestLinks() }
-    </>
+    <header className="navbar navbar-expand-lg navbar-dark bg-secondary" role="navigation">
+      <a className="navbar-brand" href="/">Reddit</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+          { uname.username ? userLinks() : guestLinks() }
+				</ul>
+      </div>
+    </header>
   );
 };
 
