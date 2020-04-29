b415bd05948b5e198d43a8351783e516d59406ad
shjang7 <suhy.jang.shj@gmail.com>
2020-04-29 14:27:25 +0900

    Add PrivateRoute for auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './common/setAuthToken';
-// import PrivateRoute from './components/routing/PrivateRoute';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import NavBar from './components/layouts/NavBar';
@@ -39,7 +39,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <Route component={NotFound} />
             </Switch>
           </section>
